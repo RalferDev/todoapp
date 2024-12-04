7162895e03063d87bdfe1c46a6efaab75b8211ac
RaffaeleCorso <r.corso@reply.it>
2024-12-04 22:09:56 +0100

    mongo atlas settings

--- DbConnection.py
+++ DbConnection.py
@@ -4,3 +4,4 @@ from motor.motor_asyncio import AsyncIOMotorClient
 client = AsyncIOMotorClient("mongodb+srv://dottcorsoraffaele:RaurVSCNgoMP1mrf@cluster0.xpffn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client.my_database
 collection = db.items
+
